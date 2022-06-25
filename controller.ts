861f0efe573a42b0b471609bf8ccf6f7c95ec5d3
Evgeny Vasilko <shadezp@gmail.com>
2022-06-25 23:07:00 +0300

    feat: add draw rectangle and square

--- controller.ts
+++ controller.ts
@@ -26,7 +26,10 @@ export function controller(command: string, arg1?: string, arg2?: string): strin
       return getMousePosition();
     case Commands.drawCircle:
       return drawCircle(Number(arg1));
-
+    case Commands.drawRectangle:
+      return drawRectangle(Number(arg1), Number(arg2));
+    case Commands.drawSquare:
+      return drawRectangle(Number(arg1), Number(arg1));
   }
   return command;
 }
@@ -86,4 +89,74 @@ function drawCircle(radius: number): string {
 
 
   return Commands.drawCircle;
+}
+
+function drawRectangle(offsetX: number, offsetY: number): string {
+  const { x, y } = robot.getMousePos();
+  const coords = {
+    a: {
+      x: x,
+      y: y,
+    },
+    b: {
+      x: x + offsetX,
+      y: y,
+    },
+    c: {
+      x: x + offsetX,
+      y: y + offsetY,
+    },
+    d: {
+      x: x,
+      y: y + offsetY,
+    }
+  }
+
+  robot.mouseToggle('down');
+
+  drawLine(coords.a.x, coords.a.y, coords.b.x, coords.b.y);
+  drawLine(coords.b.x, coords.b.y, coords.c.x, coords.c.y);
+  drawLine(coords.c.x, coords.c.y, coords.d.x, coords.d.y);
+  drawLine(coords.d.x, coords.d.y, coords.a.x, coords.a.y);
+
+  robot.mouseToggle('up');
+
+
+  return Commands.drawRectangle;
+}
+
+function drawLine(startX: number, startY: number, endX: number, endY: number): void {
+  const lengthX = Math.abs(startX - endX);
+  const lengthY = Math.abs(startY - endY);
+
+  if (startX < endX) {
+    for (let i = 0; i <= lengthX; i += 1) {
+      const x = startX + i;
+      const y = startY;
+
+      robot.dragMouse(x, y);
+    }
+  } else if (startX > endX) {
+    for (let i = 0; i <= lengthX; i += 1) {
+      const x = startX - i;
+      const y = startY;
+
+      robot.dragMouse(x, y);
+    }
+  } else if (startY < endY) {
+    for (let i = 0; i <= lengthY; i += 1) {
+      const x = startX;
+      const y = startY + i;
+
+      robot.dragMouse(x, y);
+    }
+  } else {
+    for (let i = 0; i <= lengthY; i += 1) {
+      const x = startX;
+      const y = startY - i;
+
+      robot.dragMouse(x, y);
+    }
+  }
+
 }
